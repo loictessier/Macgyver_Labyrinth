f8a7999e5bc669eab773b9ea1438535c0d29e207
loictessier <loictessier.inge@gmail.com>
2017-10-17 14:07:53 +0200

    Board.py now uses GameObject.py

--- classes/Board.py
+++ classes/Board.py
@@ -1,6 +1,7 @@
 from os import path
 import random
 import logging as log
+import GameObject as go
 
 log.basicConfig(level=log.DEBUG)
 # log.basicConfig(level=log.CRITICAL)
@@ -34,16 +35,17 @@ class Board:
 			with open(path_to_file, "r") as f:
 				for line in f:
 					for col in range(self.WIDTH_BOARD):
-						self.board_map[row][col] = self.create_object_from_key(line[col])
+						self.board_map[row][col] = self.create_object(self.OBJECTS_TYPES[line[col]])
 					row += 1
 		except FileNotFoundError as e:
 			log.critical('Error: {}'.format(e))
 
 		#Add 3 loots type objects at randoms positions
 		for it in range(3):
-			randseed = sum(row.count('p') for row in self.board_map)
-			path_counter = random.randint(0, randseed)
+			randseed = sum(row.count('p') for row in self.board_map) # count the number of positions where we can place a Loot object
+			path_counter = random.randint(0, randseed) # then select a random counter among these positions
 
+			# then convert the relative position (the nth path) into a general position (the nth positions on the map)
 			loot_pos = 0
 			for row in self.board_map:
 				for item in row:
@@ -52,23 +54,39 @@ class Board:
 							path_counter -= 1
 						loot_pos += 1
 
-			self.board_map[loot_pos // 15][(loot_pos % 15) - 1] = self.create_object_from_key("l")
-
-
+			# create a loot object at the position randomly generated
+			self.board_map[loot_pos // 15][(loot_pos % 15) - 1] = self.create_object(self.OBJECTS_TYPES["l"])
 
 	@staticmethod
-	def create_object_from_key(key):
-		#switch structure : create object corresponding to the type defined by the key parameter
+	def create_object(type_object):
+		if type_object == "Wall":
+			return go.Wall()
+		elif type_object == "Path":
+			return go.Path()
+		elif type_object == "StartPoint":
+			return go.StartPoint()
+		elif type_object == "EndPoint":
+			return go.EndPoint()
+		elif type_object == "Guard":
+			return go.Guard()
+		elif type_object == "Loot":
+			return go.Loot()
+		else:
+			log.critical('Error : object type non recognized')
 		return key
 
 	def get_object_by_coordinates(self, pos_x, pos_y):
 		return self.board_map[pos_x][pos_y]
 
+# Test
 def main():
 	my_test_board = Board()
 	my_test_board.create_board_map_from_file("level1.map")
-	print(my_test_board.board_map)
-	print("number of loots : {}.".format(sum(row.count('l') for row in my_test_board.board_map)))
+	for row in my_test_board.board_map:
+		for col in row:
+			print(col)
+		print("---")
+	# print("number of loots : {}.".format(sum(row.count('l') for row in my_test_board.board_map)))
 
 if __name__ == "__main__":
 	main()
